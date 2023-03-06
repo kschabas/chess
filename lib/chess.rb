9eef68d597a50655ab1e7f483573c11508ec5b91
Karl Schabas <karl.schabas@utoronto.ca>
2023-03-06 18:03:41 -0500

    Finish coding initial board setup

--- lib/chess.rb
+++ lib/chess.rb
@@ -26,22 +26,97 @@ class Chess
     setup_king(color)
   end
 
-  def setup_pawns(color)
-    name = "p1" + color
-    create_pawn(name, color, 'a2')
-   
+  def setup_rooks(color)
+    if color == 'W'
+      create_rook('R1W', 'W', 'a1')
+      create_rook('R2W', 'W', 'h1')
+    else
+      create_rook('R1B', 'B', 'a8')
+      create_rook('R2B', 'B', 'h8')
+    end
+  end
+
+  def setup_knights(color)
+    if color == 'W'
+      create_knight('N1W', 'W', 'b1')
+      create_knight('N2W', 'W', 'g1')
+    else
+      create_knight('N1B', 'B', 'b8')
+      create_knight('N2B', 'B', 'g8')
+    end
+  end
+
+  def setup_bishops(color)
+    if color == 'W'
+      create_bishop('B1W', 'W', 'c1')
+      create_bishop('B1W', 'W', 'f1')
+    else
+      create_bishop('B1B', 'B', 'c8')
+      create_bishop('B2B', 'B', 'f8')
+    end
+  end
+
+  def setup_queen(color)
+    if color == 'W'
+      create_queen('QW', 'W', 'd1')
+    else
+      create_queen('QB', 'B', 'd8')
+    end
+  end
+
+  def setup_king(color)
+    if color == 'W'
+      create_king('KW', 'W', 'e1')
+    else
+      create_king('KB', 'B', 'e8')
+    end
+  end
+
+  def setup_pawns(color)  
     Dim.times do |index|
-      name = "p#{index}#{color}"
-      loc = "#{(a.ord + index).chr}2"
+      name = "p#{index + 1}#{color}"
+      if color == 'W' 
+        loc = "#{('a'.ord + index).chr}2"
+      else
+        loc = "#{('a'.ord + index).chr}7"
+      end
       create_pawn(name, color, loc)
     end
-
+  end
+  
+  def add_piece(piece)
+    @piece_to_loc_hash[piece.name.to_sym] = piece
+    set_board_loc(piece.loc, piece)
   end
 
   def create_pawn(name, color, loc)
     piece = Pawn.new(name, color, loc)
-    @piece_to_loc_hash[name.to_sym] = piece
-    set_board_loc(loc, piece)
+    add_piece(piece)
+  end
+
+  def create_rook(name, color, loc)
+    piece = Rook.new(name, color, loc)
+    add_piece(piece)
+  end
+
+  def create_knight(name, color, loc)
+    piece = Knight.new(name, color, loc)
+    add_piece(piece)
+  end
+
+  def create_bishop(name, color, loc)
+    piece = Bishop.new(name, color, loc)
+    add_piece(piece)
+  end
+
+  def create_queen(name, color, loc)
+    piece = Queen.new(name, color, loc)
+    add_piece(piece)
+  end
+
+  def create_king(name, color, loc)
+    piece = King.new(name, color, loc)
+    add_piece(piece)
   end
 
   def set_board_loc(alpha_loc, piece)
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -36,7 +36,7 @@ describe Chess do
   end
   describe 'set_board_loc' do
     subject(:test_game) { described_class.new }
-    let(:test_piece) { double('Board') }
+    let(:test_piece) { double('Piece') }
     before do
       test_game.set_board_loc('a1', test_piece)
     end
@@ -45,6 +45,44 @@ describe Chess do
     end
     it 'does not assign a2' do
       expect(test_game.instance_variable_get(:@board)[1][0]).to eq(nil)
+    end
+  end
+  describe '@create_pawn' do
+    subject(:test_game) { described_class.new }
+    let(:test_piece) { double('Piece', name: 'p1W', loc: 'a1')  }
+
+    before do
+      allow(Piece).to receive(:new).and_return(test_piece)
+    end
+
+    it 'calls set_board_loc correctly' do
+      expect(test_game).to receive(:set_board_loc).with('a1',test_piece).once
+      test_game.create_pawn('p1W', 'W', 'a1')
+    end
+
+    it 'sets piece_to_loc_hash' do
+      test_game.create_pawn('p1W', 'W', 'a1')
+      expect(test_game.instance_variable_get(:@piece_to_loc_hash)[:p1W]).to eq(test_piece)
+    end
+  end
+  describe '#setup_pawns' do
+    subject(:test_game) { described_class.new }
+
+    it 'check that it create 8 pawns' do
+      expect(test_game).to receive(:create_pawn).exactly(8).times
+      test_game.setup_pawns('W')
+    end
+
+    it 'create the first pawn correctly' do
+      allow(test_game).to receive(:create_pawn)
+      expect(test_game).to receive(:create_pawn).with('p1W', 'W', 'a2').once
+      test_game.setup_pawns('W')
+    end
+
+    it 'creates last black pawn correctly' do
+      allow(test_game).to receive(:create_pawn)
+      expect(test_game).to receive(:create_pawn).with('p8B', 'B', 'h7').once
+      test_game.setup_pawns('B')
     end
   end
 end 
